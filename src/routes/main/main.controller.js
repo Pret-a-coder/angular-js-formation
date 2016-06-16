0c717c446e9c79964d0055d64b1ded9bf3caa76c
Florent Gouget <florent.gouget@gmail.com>
2016-06-16 17:49:57 +0200

    ability to post messages

--- src/routes/main/main.controller.js
+++ src/routes/main/main.controller.js
@@ -1,6 +1,11 @@
 (function () {
   'use strict'
   angular.module('demoApp').controller('MainController', [ '$scope', function ($scope) {
-    $scope.messages = ['> Bienvenue dans le chat', '@Riri: Quelqu\'un a vu Loulou ? ']
+
+    $scope.messages = [ '> Bienvenue dans le chat', '@Riri: Quelqu\'un a vu Loulou ? ' ]
+    $scope.sendMessage = function () {
+      $scope.messages.push('@Anonymous: ' + $scope.newMessage)
+      $scope.newMessage = ''
+    }
   } ])
 })()
