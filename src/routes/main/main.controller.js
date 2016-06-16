2a8b3a3a36da5b5138bb660b55c00f5f6f67e776
Florent Gouget <florent.gouget@gmail.com>
2016-06-16 17:43:36 +0200

    first binding

--- src/routes/main/main.controller.js
+++ src/routes/main/main.controller.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict'
-  angular.module('demoApp').controller('MainController', function () {
-    console.log('I am the main controller')
-  })
+  angular.module('demoApp').controller('MainController', [ '$scope', function ($scope) {
+    $scope.messages = '\n > Bienvenue dans le chat \n @Riri: Quelqu\'un a vu Loulou ? '
+  } ])
 })()
