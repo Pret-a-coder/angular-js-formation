335dd61f51e5c41b242edfd674a8fa5d42c76578
Florent Gouget <florent.gouget@gmail.com>
2016-06-17 08:39:49 +0200

    interval getting messages

--- src/routes/main/main.controller.js
+++ src/routes/main/main.controller.js
@@ -1,17 +1,26 @@
 (function () {
   'use strict'
-  angular.module('demoApp').controller('MainController', [ 'MessageService', function (MessageService) {
+  angular.module('demoApp').controller('MainController', [ '$scope', '$interval', 'MessageService', function ($scope, $interval, MessageService) {
     var self = this
 
     function init () {
-      MessageService.getMessages().then(function (messages) {
+      self.interval = $interval(getMessages, 5000)
+    }
+
+    function getMessages () {
+      return MessageService.getMessages().then(function (messages) {
         self.messages = messages
       })
     }
 
     init()
+
+    $scope.$on('$destroy', function () {
+      if (self.interval) $interval.cancel(self.interval)
+    })
+
     this.sendMessage = function () {
-      MessageService.sendMessage({ user: '@Anonymous', message: self.newMessage }).then(init)
+      MessageService.sendMessage({ user: '@Anonymous', message: self.newMessage }).then(getMessages)
       self.newMessage = ''
     }
   } ])
